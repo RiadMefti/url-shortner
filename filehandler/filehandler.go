09b1a7efe9aa3241749d9cb6162cd4c5342d1aef
RiadMefti <riadmefti@hotmail.com>
2024-10-26 14:12:35 -0400

    refactor filehandler into its own dir

---
+++ filehandler/filehandler.go
@@ -0,0 +1,46 @@
+package filehandler
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+	"strings"
+)
+
+type MainPage struct {
+	URL    string
+	NewURL string
+}
+
+type ShortenedUrlPage struct {
+	URL     string
+	NewURL  string
+	Success bool
+	Message string
+}
+
+func ServeIndex(w http.ResponseWriter, r *http.Request) {
+	tmpl, err := template.ParseFiles("templates/index.html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	tmpl.Execute(w, &MainPage{})
+}
+
+func ParseForm(w http.ResponseWriter, r *http.Request) {
+
+	inputURL := strings.TrimSpace(r.FormValue("url"))
+
+	newUrl := inputURL + "HEHEHE NEW URL"
+
+	data := ShortenedUrlPage{
+		URL:     inputURL,
+		NewURL:  newUrl,
+		Success: true,
+		Message: fmt.Sprintf("Successfully parsed URL: %s", inputURL),
+	}
+
+	tmpl, _ := template.ParseFiles("templates/url.html")
+	tmpl.Execute(w, data)
+}
--- main.go
+++ main.go
@@ -1,59 +1,19 @@
 package main
 
 import (
-	"fmt"
-	"html/template"
 	"log"
 	"net/http"
-	"strings"
+
+	"github.com/RiadMefti/url-shortner/filehandler"
 )
-
-type MainPage struct {
-	URL    string
-	NewURL string
-}
-
-type ShortenedUrlPage struct {
-	URL     string
-	NewURL  string
-	Success bool
-	Message string
-}
-
-func serveIndex(w http.ResponseWriter, r *http.Request) {
-	tmpl, err := template.ParseFiles("templates/index.html")
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	tmpl.Execute(w, &MainPage{})
-}
 
 func main() {
 	// Serve static files
 	http.Handle("GET /static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
-
 	// Route handlers
-	http.HandleFunc("GET /", serveIndex)
-	http.HandleFunc("POST /parse", parseForm)
+	http.HandleFunc("GET /", filehandler.ServeIndex)
+	http.HandleFunc("POST /parse", filehandler.ParseForm)
 
 	log.Printf("Server starting on :8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
-}
-
-func parseForm(w http.ResponseWriter, r *http.Request) {
-
-	inputURL := strings.TrimSpace(r.FormValue("url"))
-
-	newUrl := inputURL + "HEHEHE NEW URL"
-
-	data := ShortenedUrlPage{
-		URL:     inputURL,
-		NewURL:  newUrl,
-		Success: true,
-		Message: fmt.Sprintf("Successfully parsed URL: %s", inputURL),
-	}
-
-	tmpl, _ := template.ParseFiles("templates/url.html")
-	tmpl.Execute(w, data)
 }
